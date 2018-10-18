8c48880765b4d0a6f52abff8c839254113b6c08d
Alex Brown <alexdbrown2526@gmail.com>
2018-10-18 10:59:06 -0500

    multiple users able to add item to shelf

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -14,8 +14,10 @@ router.get('/', (req, res) => {
  * Add an item for the logged in user to the shelf
  */
 router.post('/', (req, res) => {
+    console.log(req.user);
+    
         pool.query(`INSERT INTO "item" ("description", "image_url", "person_id")
-        VALUES ($1, $2, $3)`, [req.body.description, req.body.image_url, 1])
+        VALUES ($1, $2, $3)`, [req.body.description, req.body.image_url, req.user.id])
 });
 
 
